9adb9168e1bf2427be4c1ae1051665a5845a5187
Dloed <96358595+Dloed@users.noreply.github.com>
2024-09-19 00:59:36 +0200

    some corrections

--- src/components/simpleComponents/NewsCard.jsx
+++ src/components/simpleComponents/NewsCard.jsx
@@ -1,15 +1,29 @@
-
 import "./NewsCard.css";
-import { React } from "react";
+
+// Вспомогательная функция для обрезки текста по целым словам
+const truncateText = (text, maxLength) => {
+  if (text.length <= maxLength) {
+    return text;
+  }
+
+  // Разбиваем текст на слова и собираем до тех пор, пока не достигнем maxLength
+  const truncated = text.split(' ').reduce((acc, word) => {
+    if (acc.length + word.length + 1 <= maxLength) {
+      return acc + (acc ? ' ' : '') + word;
+    }
+    return acc;
+  }, '');
+
+  return truncated + "...";
+};
 
 const NewsCard = ({ className, title, imgUrl, text, source, sourceText }) => {
   return (
     <div className={className}>
       <div className="title">{title}</div>
-      <img className="img" src={imgUrl} alt={title}></img>
-      <div className="text">
-        <span className="text">{text}</span>
-      </div>
+      <img className="img" src={imgUrl} alt={`Изображение для новости: ${title}`}></img>
+   
+      <div className="text">{truncateText(text, 160)}</div>
       <a className="source" href={source}>
         {sourceText}
       </a>
@@ -18,3 +32,7 @@ const NewsCard = ({ className, title, imgUrl, text, source, sourceText }) => {
 };
 
 export default NewsCard;
+
+
+
+
